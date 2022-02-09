0cc0f80a8e58f1115a5e5e9e6e15dfbd98810a6e
백승민 <conoa8@gmail.com>
2022-02-09 12:52:38 +0900

    승민 | 추가 : FairyDBHelper 클래스 추가, Const 클래스에 SQL문 (DB 저장, 불러오기) 추가 완료

--- app/src/main/java/com/jihoon/fairy/Const/Const.java
+++ app/src/main/java/com/jihoon/fairy/Const/Const.java
@@ -1,4 +1,33 @@
 package com.jihoon.fairy.Const;
 
 public class Const {
+    private Const() {};
+
+    public static final String TBL_EMOTIONS = "EMOTIONS_T";
+    public static final String COL_NO = "NO";
+    public static final String COL_TIME = "REGISTRATION_TIME";
+    public static final String COL_ANGER = "ANGER_DEGREE";
+    public static final String COL_CONTEMPT = "CONTEMPT_DEGREE";
+    public static final String COL_DISGUST = "DISGUST_DEGREE";
+    public static final String COL_FEAR = "FEAR_DEGREE";
+    public static final String COL_HAPPINESS = "HAPPINESS_DEGREE";
+    public static final String COL_NEUTRAL = "NEUTRAL_DEGREE";
+    public static final String COL_SADNESS = "SADNESS_DEGREE";
+    public static final String COL_SURPRISE = "SURPRISE_DEGREE";
+
+    // CREATE TABLE IF NOT EXISTS EMOTIONS_T (TITLE TEXT NOT NULL)
+    public static final String SQL_CREATE_TBL_EMOTIONS = String.format("CREATE TABLE IF NOT EXISTS %s " +
+                    "(%s INTEGER PRIMARY KEY AUTOINCREMENT, %s DATETIME, %s DOUBLE, %s DOUBLE, %s DOUBLE, %s DOUBLE, " +
+                    "%s DOUBLE, %s DOUBLE, %s DOUBLE, %s DOUBLE",
+            TBL_EMOTIONS, COL_NO, COL_TIME, COL_ANGER, COL_CONTEMPT, COL_DISGUST,
+            COL_FEAR, COL_HAPPINESS, COL_NEUTRAL, COL_SADNESS, COL_SURPRISE);
+
+    // DB 저장
+    public static final String SQL_INSERT_TBL_EMOTIONS = String.format("INSERT OR REPLACE INTO %s " +
+                    "(%s, %s, %s, %s, %s, %s, %s, %s, %s) VALUES",
+            TBL_EMOTIONS, COL_TIME, COL_ANGER, COL_CONTEMPT, COL_DISGUST,
+            COL_FEAR, COL_HAPPINESS, COL_NEUTRAL, COL_SADNESS, COL_SURPRISE);
+
+    // DB 불러오기
+    public static final String SQL_SELECT_TBL_EMOTIONS = String.format("SELECT * FROM %s ",TBL_EMOTIONS);
 }
---
+++ app/src/main/java/com/jihoon/fairy/FairyDBHelper.java
@@ -0,0 +1,27 @@
+package com.jihoon.fairy;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.jihoon.fairy.Const.Const;
+
+public class FairyDBHelper extends SQLiteOpenHelper {
+
+    public static final int DB_VERSION = 1;
+    public static final String DBFILE_CONTACT = "fairyDB.db";
+
+    public FairyDBHelper(Context context) {
+        super(context, DBFILE_CONTACT, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Const.SQL_CREATE_TBL_EMOTIONS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onCreate(db);
+    }
+}
