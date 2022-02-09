296c03932bbb6e87b4087997c93f17768757c9f6
백승민 <conoa8@gmail.com>
2022-02-09 16:50:02 +0900

    승민 | 추가 : DB 매니저 클래스에 저장, 불러오기 기능 추가

--- app/src/main/java/com/jihoon/fairy/DB/FairyDBManager.java
+++ app/src/main/java/com/jihoon/fairy/DB/FairyDBManager.java
@@ -2,18 +2,44 @@ package com.jihoon.fairy.DB;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import com.jihoon.fairy.Const.Const;
+import com.jihoon.fairy.R;
 
 public class FairyDBManager {
 
+    // 데이터 저장(INSERT INTO)
+    public void save_values(FairyDBHelper fairyDBHelper, String Date, String Time,
+                            Double happiness, Double neutral, Double sadness) {
+        SQLiteDatabase db = fairyDBHelper.getWritableDatabase() ;
+
+        String RegistrationDate = Date;
+        String RegistrationTime = Time;
+        Double happinessDegree = happiness;
+        Double neutralDegree = neutral;
+        Double sadnessDegree = sadness;
+
+        String sqlInsert = Const.SQL_INSERT_TBL_EMOTIONS +
+                " (" +
+                "'" + RegistrationDate + "', " +
+                "'" + RegistrationTime + "', " +
+                "" + happinessDegree + ", " +
+                "" + neutralDegree + ", " +
+                "" + sadnessDegree + ", " +
+                ")" ;
+
+        db.execSQL(sqlInsert) ;
+    }
+
+    // 데이터 조회(SELECT)
     public void load_values(FairyDBHelper fairyDBHelper) {
 
         SQLiteDatabase db = fairyDBHelper.getReadableDatabase();
         Cursor cursor = db.rawQuery(Const.SQL_SELECT_TBL_EMOTIONS, null);
 
         if (cursor.moveToFirst()) {
-            // COL_NO 값 가져오기
             String RegistrationDate = cursor.getString(1);
             String RegistrationTime = cursor.getString(2);
             Double happinessDegree = cursor.getDouble(3);
