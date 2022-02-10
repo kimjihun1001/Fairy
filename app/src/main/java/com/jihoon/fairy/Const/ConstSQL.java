483cbdd81d56fbf88ef50796d79d96cd351b9e2a
백승민 <conoa8@gmail.com>
2022-02-10 14:26:50 +0900

    승민 | 수정 : ConstSQL 클래스 수정

--- app/src/main/java/com/jihoon/fairy/Const/ConstSQL.java
+++ app/src/main/java/com/jihoon/fairy/Const/ConstSQL.java
@@ -5,26 +5,29 @@ public class ConstSQL {
 
     public static final String TBL_EMOTIONS = "EMOTIONS_T";
     public static final String COL_NO = "NO";
-    public static final String COL_DATE = "REGISTRATION_DATE";
-    public static final String COL_TIME = "REGISTRATION_TIME";
+    public static final String COL_DATETIME = "REGISTRATION_DATETIME";
     public static final String COL_HAPPINESS = "HAPPINESS_DEGREE";
     public static final String COL_SADNESS = "SADNESS_DEGREE";
     public static final String COL_NEUTRAL = "NEUTRAL_DEGREE";
+    public static final String COL_IMAGEPATH = "IMAGEPATH";
+    public static final String COL_IMAGENAME = "IMAGENAME";
+
 
     // CREATE TABLE IF NOT EXISTS EMOTIONS_T (TITLE TEXT NOT NULL)
     public static final String SQL_CREATE_TBL = "CREATE TABLE IF NOT EXISTS " + TBL_EMOTIONS + " " +
             "(" +
             COL_NO +        " INTEGER PRIMARY KEY AUTOINCREMENT" + ", " +
-            COL_DATE +      " TEXT"                              + ", " +
-            COL_TIME +      " TEXT"                              + ", " +
+            COL_DATETIME +  " TEXT"                              + ", " +
             COL_HAPPINESS + " DOUBLE"                            + ", " +
             COL_SADNESS +   " DOUBLE"                            + ", " +
-            COL_NEUTRAL +   " DOUBLE"                            + ")";
+            COL_NEUTRAL +   " DOUBLE"                            + ", " +
+            COL_IMAGEPATH + " DOUBLE"                            + ", " +
+            COL_IMAGENAME + " DOUBLE"                            + ")";
 
     // DB 저장
     public static final String SQL_INSERT_TBL_EMOTIONS = "INSERT OR REPLACE INTO " + TBL_EMOTIONS + " " +
             "(" +
-            COL_DATE + ", " + COL_TIME + ", " + COL_HAPPINESS + ", " + COL_NEUTRAL + ", " + COL_SADNESS + ") VALUES ";
+            COL_DATETIME + ", " + COL_HAPPINESS + ", " + COL_NEUTRAL + ", " + COL_SADNESS + ", " + COL_IMAGEPATH + ", " + COL_IMAGENAME + ") VALUES ";
 
     // DB 불러오기
     public static final String SQL_SELECT_TBL_EMOTIONS = "SELECT * FROM " + TBL_EMOTIONS;
