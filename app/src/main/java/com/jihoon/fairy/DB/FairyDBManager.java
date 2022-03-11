20fe4e61fd6fd45522f6e3b238ff2d33aa052a16
kimjihun1001 <kimjihun1001@gmail.com>
2022-03-11 09:33:23 +0900

    Revert "[제주더큰내일센터 APK 배포 버전]"
    This reverts commit afe960e1993ce3793eb4b2464b2b6068c3bb11ad.

--- app/src/main/java/com/jihoon/fairy/DB/FairyDBManager.java
+++ app/src/main/java/com/jihoon/fairy/DB/FairyDBManager.java
@@ -8,7 +8,6 @@ import androidx.annotation.RequiresApi;
 
 import com.jihoon.fairy.Const.Const;
 import com.jihoon.fairy.Const.ConstSQL;
-import com.jihoon.fairy.MainActivity;
 import com.jihoon.fairy.Model.ModelEmotions;
 import com.jihoon.fairy.Model.ModelUserData;
 
